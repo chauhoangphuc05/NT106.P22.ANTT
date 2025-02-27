c5277f65046a3e034d3a5e147fce65ca632f3af8
Châu Hoàng Phúc <144977826+chauhoangphuc05@users.noreply.github.com>
2025-02-27 16:47:43 +0700

    test
    test

--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -2,9 +2,7 @@ namespace Monopoly
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
